af32ea257333a641870b6db3c6ef6a3048a60c76
Michal Buczko <michal.buczko@gmail.com>
2020-03-01 18:35:25 +0100

    refactor: move CLI config into separate yaml

--- src/main.rs
+++ src/main.rs
@@ -2,168 +2,28 @@ mod db;
 mod utils;
 mod vault;
 
-use crate::vault::link::Link;
-use crate::vault::auth::Authentication;
 use crate::utils::read_file;
+use crate::vault::auth::Authentication;
+use crate::vault::link::Link;
+use crate::vault::vault::{init_vault, Vault};
 
-use clap::{App, Arg, ArgMatches, SubCommand};
+use clap::{App, ArgMatches, load_yaml};
 use log::Level;
 use miniserde::json;
 use semver::Version;
 use std::process::exit;
-use crate::vault::vault::{init_vault, Vault};
-
 
 const VERSION: &'static str = env!("CARGO_PKG_VERSION");
 
 fn main() {
     simple_logger::init_with_level(Level::Debug).unwrap();
 
-    let matches = App::new("Świnka skarbonka na linki")
-        .version(VERSION)
-        .about("Saves your precious links into local vault")
-        .arg(
-            Arg::with_name("database")
-                .help("database to use")
-                .short("d")
-                .long("db")
-                .takes_value(true),
-        )
-        .subcommand(
-            SubCommand::with_name("add")
-                .about("Adds a new link")
-                .arg(
-                    Arg::with_name("url")
-                        .help("link to store in database")
-                        .required(true),
-                )
-                .arg(
-                    Arg::with_name("user")
-                        .help("an owner of stored link")
-                        .short("u")
-                        .long("user")
-                        .takes_value(true),
-                )
-                .arg(
-                    Arg::with_name("password")
-                        .help("owner's password")
-                        .short("p")
-                        .long("pass")
-                        .takes_value(true),
-                )
-                .arg(
-                    Arg::with_name("description")
-                        .help("optional description")
-                        .short("d")
-                        .long("desc")
-                        .takes_value(true),
-                )
-                .arg(
-                    Arg::with_name("tags")
-                        .help("optional tags assigned to the link")
-                        .short("t")
-                        .long("tags")
-                        .use_delimiter(true)
-                        .takes_value(true),
-                ),
-        )
-        .subcommand(
-            SubCommand::with_name("import")
-                .about("Import links from JSON file")
-                .arg(
-                    Arg::with_name("file")
-                        .help("JSON file to import")
-                        .required(true),
-                )
-                .arg(
-                    Arg::with_name("user")
-                        .help("an owner of stored link")
-                        .short("u")
-                        .long("user")
-                        .takes_value(true),
-                )
-                .arg(
-                    Arg::with_name("password")
-                        .help("owner's password")
-                        .short("p")
-                        .long("pass")
-                        .takes_value(true),
-                ),
-        )
-        .subcommand(
-            SubCommand::with_name("ls")
-                .about("Lists matching links")
-                .arg(Arg::with_name("url").help("link or its part to match"))
-                .arg(
-                    Arg::with_name("description")
-                        .help("optional part of description to match")
-                        .short("d")
-                        .long("desc")
-                        .takes_value(true),
-                )
-                .arg(
-                    Arg::with_name("user")
-                        .help("an owner of stored link")
-                        .short("u")
-                        .long("user")
-                        .takes_value(true),
-                )
-                .arg(
-                    Arg::with_name("tags")
-                        .help("optional comma-separated tags to match")
-                        .short("t")
-                        .long("tags")
-                        .use_delimiter(true)
-                        .takes_value(true),
-                ),
-        )
-        .subcommand(
-            SubCommand::with_name("users")
-                .about("Manage with users")
-                .subcommand(
-                    SubCommand::with_name("add")
-                        .about("Adds a new user")
-                        .arg(
-                            Arg::with_name("user")
-                                .help("User's identifier (login)")
-                                .required(true),
-                        )
-                        .arg(
-                            Arg::with_name("password")
-                                .help("user's password")
-                                .short("p")
-                                .long("pass")
-                                .takes_value(true),
-                        ),
-                )
-                .subcommand(
-                    SubCommand::with_name("passwd")
-                        .about("Changes user's password")
-                        .arg(
-                            Arg::with_name("user")
-                                .help("User's identifier (login)")
-                                .required(true),
-                        )
-                        .arg(
-                            Arg::with_name("password")
-                                .help("user's new password")
-                                .short("p")
-                                .long("pass")
-                                .takes_value(true),
-                        ),
-                )
-                .subcommand(
-                    SubCommand::with_name("ls")
-                        .about("Lists matching users")
-                        .arg(Arg::with_name("login").help("User's identifier pattern to list")),
-                ),
-        )
-        .get_matches();
-
+    let yaml = load_yaml!("cli.yml");
+    let matches = App::from(yaml).get_matches();
     let db = matches.value_of("database").unwrap_or("links.db");
     match init_vault(db, Version::parse(VERSION).unwrap()) {
         Ok(v) => process_command(v, matches),
-        _ => panic!("cannot initialize database"),
+        _ => panic!("Cannot initialize database"),
     }
 }
 
@@ -217,7 +77,7 @@ fn process_command(mut vault: Vault, matches: ArgMatches) {
                     exit(1);
                 }
             },
-            ("ls", Some(sub_m)) => match vault.match_users(sub_m.value_of("login")) {
+            ("ls", Some(sub_m)) => match vault.match_users(sub_m.value_of("user")) {
                 Ok(users) => {
                     for (user, count) in users {
                         println!("{} ({})", user, count);
